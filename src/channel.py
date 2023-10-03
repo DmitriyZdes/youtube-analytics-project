669cd6084965a61e6e9636602c1c92d829e6d826
Дмитрий <dfeskov1998@mail.ru>
2023-10-03 22:14:31 +0300

    add new methods and attributes

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,4 @@
+import json
 import os
 from googleapiclient.discovery import build
 
@@ -8,12 +9,27 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        channel = self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.url = f'https://www.youtube.com/channel/{channel["items"][0]["id"]}'
+        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+        self.total_views = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
 
-        api_key: str = os.getenv('API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        data = youtube.channels().list(id="UC44oy3QadzoUZt7G0DDf5DQ", part='snippet,statistics,contentDetails').execute()
+
+        data = self.get_service().channels().list(id="UC44oy3QadzoUZt7G0DDf5DQ", part='snippet,statistics,contentDetails').execute()
         print(data)
 
+    @classmethod
+    def get_service(cls):
+        _api_key: str = os.getenv('API_KEY')
+        youtube = build('youtube', 'v3', developerKey=_api_key)
+        return youtube
+    def to_json(self, filename):
+        dict_to_json = {"channel_id": self.channel_id, "title": self.title, "description": self.description, "url": self.url, "subscriber": self.subscriber_count, "total_views": self.total_views}
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(dict_to_json, file)
