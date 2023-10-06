7760c773b1fc2abfc48f107bfefbbc432259b249
Дмитрий <dfeskov1998@mail.ru>
2023-10-07 00:20:59 +0300

    add str method and methods of additing, substracting and comparing

--- src/channel.py
+++ src/channel.py
@@ -8,6 +8,7 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+
         self.channel_id = channel_id
         channel = self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
         self.title = channel['items'][0]['snippet']['title']
@@ -17,19 +18,51 @@ class Channel:
         self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
         self.total_views = channel['items'][0]['statistics']['viewCount']
 
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-
 
         data = self.get_service().channels().list(id="UC44oy3QadzoUZt7G0DDf5DQ", part='snippet,statistics,contentDetails').execute()
         print(data)
 
+
     @classmethod
     def get_service(cls):
+        """Возвращает объект для работы с YouTube API"""
+
         _api_key: str = os.getenv('API_KEY')
         youtube = build('youtube', 'v3', developerKey=_api_key)
         return youtube
+
+
     def to_json(self, filename):
+        """Сохраняет атрибуты экземпляра в файл json"""
+
         dict_to_json = {"channel_id": self.channel_id, "title": self.title, "description": self.description, "url": self.url, "subscriber": self.subscriber_count, "total_views": self.total_views}
         with open(filename, 'w', encoding='utf-8') as file:
             json.dump(dict_to_json, file)
+
+
+    def __str__(self):
+        """Информация о название канала и его ссылка"""
+
+        return f'{self.title} {self.url}'
+
+
+    def __add__(self, other):
+        """Сложение количества подписчиков каналов"""
+
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+
+    def __sub__(self, other):
+        """Вычитание количества подписчиков каналов"""
+
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+
+    def __eq__(self, other):
+        """Сравнивает количество подписчиков каналов"""
+
+        return self.subscriber_count == other.subscriber_count
+
