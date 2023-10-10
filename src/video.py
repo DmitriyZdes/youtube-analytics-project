75ada478b83206482be97c67e5a26cddf5b1871b
Дмитрий <dfeskov1998@mail.ru>
2023-10-10 23:18:57 +0300

    create class and subclass

---
+++ src/video.py
@@ -0,0 +1,23 @@
+import os
+from googleapiclient.discovery import build
+
+class Video:
+
+    api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id: str) -> None:
+        self.video_id = video_id
+        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',id=video_id).execute()
+        self.video_title = video_response['items'][0]['snippet']['title']
+        self.video_url = f'https://www.youtube.com/watch?v={video_id}'
+        self.view_count = video_response['items'][0]['statistics']['viewCount']
+        self.likes_count = video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return f'{self.video_title}'
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
