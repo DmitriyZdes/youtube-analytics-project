9beaa96dacccf33eabd54f539dbe6cb4a12fef8a
Дмитрий <dfeskov1998@mail.ru>
2023-10-22 13:38:06 +0300

    add try/except block

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 class Video:
 
@@ -8,11 +9,24 @@ class Video:
 
     def __init__(self, video_id: str) -> None:
         self.video_id = video_id
-        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',id=video_id).execute()
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.video_url = f'https://www.youtube.com/watch?v={video_id}'
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.likes_count = video_response['items'][0]['statistics']['likeCount']
+        try:
+            video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                        id=video_id).execute()
+            if 'items' in video_response and video_response['items']:
+                self.video_title = video_response['items'][0]['snippet']['title']
+                self.video_url = f'https://www.youtube.com/watch?v={video_id}'
+                self.view_count = video_response['items'][0]['statistics']['viewCount']
+                self.likes_count = video_response['items'][0]['statistics']['likeCount']
+            else:
+                self.video_title = None
+                self.likes_count = None
+                self.view_count = None
+                self.video_url = None
+        except HttpError:
+                self.video_title = None
+                self.video_url = None
+                self.view_count = None
+                self.likes_count = None
 
     def __str__(self):
         return f'{self.video_title}'
